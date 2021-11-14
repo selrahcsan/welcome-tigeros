6cf653bbc28866a76dff9ed908eed15d20ca7e61
Natanael <natanaelgplay@gmail.com>
2021-11-14 12:29:21 -0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+[ ! "${EUID}" = "0" ] && {
+  echo "Execute esse script como root:"
+  echo
+  echo "  sudo ${0}"
+  echo
+  exit 1
+}
+
+HERE="$(dirname "$(readlink -f "${0}")")"
+
+working_dir=$(mktemp -d)
+
+cp -rfv "${HERE}"/* ${working_dir}
+rm ${working_dir}/build.sh
+rm ${working_dir}/README.md 
+
+dpkg -b ${working_dir}
+rm -rfv ${working_dir}
+
+mv "${working_dir}.deb" "${HERE}/welcome.deb"
+
+chmod 777 "${HERE}/welcome.deb"
+chmod -x  "${HERE}/welcome.deb"
