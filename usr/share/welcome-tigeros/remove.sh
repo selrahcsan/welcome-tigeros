1ebbc9b907b632eea24456733f3fdac48d055a1f
Natanael <natanaelgplay@gmail.com>
2023-03-28 16:47:59 -0300

    Update remove.sh

--- usr/share/welcome-tigeros/remove.sh
+++ usr/share/welcome-tigeros/remove.sh
@@ -8,6 +8,11 @@
 # Tecnologias utilizadas: bash, html, css, javascript e <3
 # Script by Natanael.755
 
+WEBAPP_TARGET="${HOME}/.local/share"
+
+mkdir -p "${WEBAPP_TARGET}/bin"
+mkdir -p "${WEBAPP_TARGET}/applications"
+
 function flatpak-remove-gui() {
   name=$(echo "${1}" | cut -c 20-)
   flatpak uninstall --assumeyes "${2}" | yad --center --progress --pulsate --auto-close --no-buttons --borders=32 --width=480 --text="<b>Removendo ${name}</b>" --on-top --skip-taskbar --undecorated
@@ -20,8 +25,8 @@ windowID="$(xwininfo -name "Bem-vindo TigerOS" | head -n2 | tail -n1 | awk '{pri
 
 case $1 in
     msoffice)
-        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm /usr/bin/msword /usr/bin/msexcel /usr/bin/mspowerpoint \
-        /usr/share/applications/msword.desktop /usr/share/applications/msexcel.desktop /usr/share/applications/mspowerpoint.desktop && {
+        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm ${WEBAPP_TARGET}/bin/msword ${WEBAPP_TARGET}/bin/msexcel ${WEBAPP_TARGET}/bin/mspowerpoint \
+        ${WEBAPP_TARGET}/applications/msword.desktop ${WEBAPP_TARGET}/applications/msexcel.desktop ${WEBAPP_TARGET}/applications/mspowerpoint.desktop && {
         rm -r ~/.msword ~/.msexcel ~/.mspowerpoint
         zenity --modal --info --width=380 --attach="$windowID" --text="O MS Office foi removido com sucesso!"
     } || {
@@ -31,8 +36,8 @@ case $1 in
     ;;
 
     googledocs)
-        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm /usr/bin/gdocs /usr/bin/gsheets /usr/bin/gslides \
-        /usr/share/applications/gdocs.desktop /usr/share/applications/gsheets.desktop /usr/share/applications/gslides.desktop && {
+        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm ${WEBAPP_TARGET}/bin/gdocs ${WEBAPP_TARGET}/bin/gsheets ${WEBAPP_TARGET}/bin/gslides \
+        ${WEBAPP_TARGET}/applications/gdocs.desktop ${WEBAPP_TARGET}/applications/gsheets.desktop ${WEBAPP_TARGET}/applications/gslides.desktop && {
         rm -r ~/.gdocs ~/.gsheets ~/.gslides
         zenity --modal --info --width=380 --attach="$windowID" --text="O Google Docs foi removido com sucesso!"
     } || {
@@ -42,7 +47,7 @@ case $1 in
     ;;
 
     gcontacts)
-        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm /usr/bin/gcontacts /usr/share/applications/gcontacts.desktop && {
+        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm ${WEBAPP_TARGET}/bin/gcontacts ${WEBAPP_TARGET}/applications/gcontacts.desktop && {
             rm -r ~/.gcontacts
             zenity --info --text="O webapp Google Contacts foi removido com sucesso!" --modal --attach="$windowID" --width=380
         } || {
@@ -52,7 +57,7 @@ case $1 in
         ;;
 
     gcalendar)
-        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm /usr/bin/gcalendar /usr/share/applications/gcalendar.desktop && {
+        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm ${WEBAPP_TARGET}/bin/gcalendar ${WEBAPP_TARGET}/applications/gcalendar.desktop && {
             rm -r ~/.gcalendar
             zenity --info --text="O webapp Google Calendar foi removido com sucesso!" --modal --attach="$windowID" --width=380
         } || {
@@ -62,7 +67,7 @@ case $1 in
         ;;
 
     gkeep)
-        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm /usr/bin/gkeep /usr/share/applications/gkeep.desktop && {
+        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm ${WEBAPP_TARGET}/bin/gkeep ${WEBAPP_TARGET}/applications/gkeep.desktop && {
             rm -r ~/.gkeep
             zenity --info --text="O webapp Google Keep foi removido com sucesso!" --modal --attach="$windowID" --width=380
         } || {
@@ -72,7 +77,7 @@ case $1 in
         ;;
 
     cvyou)
-        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm /usr/bin/cvyou /usr/share/applications/cvyou.desktop && {
+        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY ${WEBAPP_TARGET}/bin/cvyou ${WEBAPP_TARGET}/applications/cvyou.desktop && {
             rm -r ~/.cvyou
             zenity --info --text="O webapp CV2You foi removido com sucesso!" --modal --attach="$windowID" --width=380
         } || {
@@ -82,7 +87,7 @@ case $1 in
         ;;
 
     github)
-        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm /usr/bin/github /usr/share/applications/github.desktop && {
+        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm ${WEBAPP_TARGET}/bin/github ${WEBAPP_TARGET}/applications/github.desktop && {
             rm -r ~/.github
             zenity --info --text="O webapp GitHub foi removido com sucesso!" --modal --attach="$windowID" --width=380
         } || {
@@ -92,7 +97,7 @@ case $1 in
         ;;
 
     gist)
-        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm /usr/bin/gist /usr/share/applications/gist.desktop && {
+        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm ${WEBAPP_TARGET}/bin/gist ${WEBAPP_TARGET}/applications/gist.desktop && {
             rm -r ~/.gist
             zenity --info --text="O webapp Gist foi removido com sucesso!" --modal --attach="$windowID" --width=380
         } || {
@@ -102,7 +107,7 @@ case $1 in
         ;;
 
     glitch)
-        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm /usr/bin/glitch /usr/share/applications/glitch.desktop && {
+        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm ${WEBAPP_TARGET}/bin/glitch ${WEBAPP_TARGET}/applications/glitch.desktop && {
             rm -r ~/.glitch
             zenity --info --text="O webapp Glitch foi removido com sucesso!" --modal --attach="$windowID" --width=380
         } || {
@@ -112,7 +117,7 @@ case $1 in
         ;;
 
     codepen)
-        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm /usr/bin/codepen /usr/share/applications/codepen.desktop && {
+        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm ${WEBAPP_TARGET}/bin/codepen ${WEBAPP_TARGET}/applications/codepen.desktop && {
             rm -r ~/.codepen
             zenity --info --text="O webapp CodePen foi removido com sucesso!" --modal --attach="$windowID" --width=380
         } || {
@@ -122,7 +127,7 @@ case $1 in
         ;;
 
     diagrams)
-        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm /usr/bin/diagrams /usr/share/applications/diagrams.desktop && {
+        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm ${WEBAPP_TARGET}/bin/diagrams ${WEBAPP_TARGET}/applications/diagrams.desktop && {
             rm -r ~/.diagrams
             zenity --info --text="O webapp Diagrams foi removido com sucesso!" --modal --attach="$windowID" --width=380
         } || {
@@ -132,7 +137,7 @@ case $1 in
         ;;
 
     gforms)
-        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm /usr/bin/gforms /usr/share/applications/gforms.desktop && {
+        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm ${WEBAPP_TARGET}/bin/gforms ${WEBAPP_TARGET}/applications/gforms.desktop && {
             rm -r ~/.gforms
             zenity --info --text="O webapp Google Forms foi removido com sucesso!" --modal --attach="$windowID" --width=380
         } || {
@@ -142,7 +147,7 @@ case $1 in
         ;;
 
     spark)
-        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm /usr/bin/spark /usr/share/applications/spark.desktop && {
+        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm ${WEBAPP_TARGET}/bin/spark ${WEBAPP_TARGET}/applications/spark.desktop && {
             rm -r ~/.spark
             zenity --info --text="O webapp Adobe Spark foi removido com sucesso!" --modal --attach="$windowID" --width=380
         } || {
@@ -152,7 +157,7 @@ case $1 in
         ;;
 
     canva)
-        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm /usr/bin/canva /usr/share/applications/canva.desktop && {
+        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm ${WEBAPP_TARGET}/bin/canva ${WEBAPP_TARGET}/applications/canva.desktop && {
             rm -r ~/.canva
             zenity --info --text="O webapp Canva foi removido com sucesso!" --modal --attach="$windowID" --width=380
         } || {
@@ -162,7 +167,7 @@ case $1 in
         ;;
 
     logomaker)
-        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm /usr/bin/logomaker /usr/share/applications/logomaker.desktop && {
+        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm ${WEBAPP_TARGET}/bin/logomaker ${WEBAPP_TARGET}/applications/logomaker.desktop && {
             rm -r ~/.logomaker
             zenity --info --text="O webapp Logo Maker foi removido com sucesso!" --modal --attach="$windowID" --width=380
         } || {
@@ -172,7 +177,7 @@ case $1 in
         ;;
 
     photoshop)
-        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm /usr/bin/photoshop /usr/share/applications/photoshop.desktop && {
+        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm ${WEBAPP_TARGET}/bin/photoshop ${WEBAPP_TARGET}/applications/photoshop.desktop && {
             rm -r ~/.photoshop
             zenity --info --text="O webapp Photoshop Online foi removido com sucesso!" --modal --attach="$windowID" --width=380
         } || {
@@ -182,7 +187,7 @@ case $1 in
         ;;
 
     photopea)
-        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm /usr/bin/photopea /usr/share/applications/photopea.desktop && {
+        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm ${WEBAPP_TARGET}/bin/photopea ${WEBAPP_TARGET}/applications/photopea.desktop && {
             rm -r ~/.photopea
             zenity --info --text="O webapp Photopea foi removido com sucesso!" --modal --attach="$windowID" --width=380
         } || {
@@ -192,7 +197,7 @@ case $1 in
         ;;
 
     gmeet)
-        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm /usr/bin/gmeet /usr/share/applications/gmeet.desktop && {
+        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm ${WEBAPP_TARGET}/bin/gmeet ${WEBAPP_TARGET}/applications/gmeet.desktop && {
             rm -r ~/.gmeet
             zenity --info --text="O webapp Google Meet foi removido com sucesso!" --modal --attach="$windowID" --width=380
         } || {
@@ -202,7 +207,7 @@ case $1 in
         ;;
 
     medium)
-        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm /usr/bin/medium /usr/share/applications/medium.desktop && {
+        pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY rm ${WEBAPP_TARGET}/bin/medium ${WEBAPP_TARGET}/applications/medium.desktop && {
             rm -r ~/.medium
             zenity --info --text="O webapp Medium foi removido com sucesso!" --modal --attach="$windowID" --width=380
         } || {
